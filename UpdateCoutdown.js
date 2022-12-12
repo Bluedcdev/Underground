8971165f4bf7ef17af96551f6d40fac78530a365
Bluedcdev <85186004+Bluedcdev@users.noreply.github.com>
2022-12-12 17:18:29 +0100

    Add files via upload

---
+++ UpdateCoutdown.js
@@ -0,0 +1,20 @@
+const countDate = new Date("Sep 29 2022 20:00:00").getTime();
+
+var x = setInterval(function(){
+    let now = new Date().getTime();
+
+    let distance = countDate - now;
+
+    var d = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hrs = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 *60));
+    var min = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var sec = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('timer').innerHTML = d + "d " + hrs + "h " + min + "m " + sec + "s ";
+
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementById('Text1').innerHTML = "Update is Out!";
+        document.getElementById('timer').innerHTML = "Take a look at the new Update";
+    }
+})
---
+++ script.js
@@ -0,0 +1,51 @@
+const countDate = new Date("Dec 12 2022 18:00:00").getTime();
+
+var x = setInterval(function(){
+    let now = new Date().getTime();
+
+    let distance = countDate - now;
+
+    var d = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hrs = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 *60));
+    var min = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var sec = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('timer').innerHTML = d + "d " + hrs + "h " + min + "m " + sec + "s ";
+
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementById('Text1').innerHTML = "You can play it soon!";
+        document.getElementById('timer').innerHTML = "The Underground chapter 1 is released!";
+    }
+})
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
